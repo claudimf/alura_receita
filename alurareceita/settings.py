af43fc322baeea5c6c65bc7840562cf765a2fc25
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-02-24 15:20:10 -0300

    Makes class 'Criando o app de usuários'

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'receitas',
-    'pessoas'
+    'pessoas',
+    'usuarios'
 ]
 
 MIDDLEWARE = [
---
+++ usuarios/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+
+
+def cadastro(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def dashboard(request):
+    pass
+
+
+def logout(request):
+    pass
